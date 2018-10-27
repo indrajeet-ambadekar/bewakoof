6e945d5c124a49bb52452c5f3dfc5a108e944fe9
indrajeet.ambadekar <indrajeet.ambadekar@docsapp.in>
2018-10-27 14:48:07 +0530

    assignment complete

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# bewakoof
+# Bewakoof Assignment
 
-Coding skills test
+**author:** _Indrajeet_ 
+
+> A ReactJs project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:5000
+npm run start
+
+# Run a node backend server on locahost:9000
+node server.js
+### I used a node backend server because the serapi apis dont allow cross-domain requests.
+```
+
+
+
+
+## Libraries Used
+```
+1. Material Design: as a css layouting framework
+2. Google Search Javascript SDK: Using "serapi"
+
+```
--- src/components/views/search.js
+++ src/components/views/search.js
@@ -29,6 +29,13 @@ class Main extends Component {
 
     return urlParams;
   };
+  isArray = a => {
+    return !!a && a.constructor === Array;
+  };
+
+  isObject = a => {
+    return !!a && a.constructor === Object;
+  };
   componentDidMount() {
     let keyword = this.parseQueryString()["q"];
     let self = this;
@@ -44,22 +51,82 @@ class Main extends Component {
   }
 
   parseNestedObj = tree => {
-    console.log(tree);
     let rows = [];
     for (var key in tree) {
-      console.log(key, tree[key]);
-      if (key == "image") {
+      if (this.isArray(tree[key]) && key != "extensions") {
         rows.push(
-          <div key={Math.random()} className="col s12">
-            <img className="posterImg" src={tree[key]} width="100px" />
+          <span key={Math.random()}>
+            <b>{key} :</b>
+          </span>
+        );
+        let items = [];
+        for (var j in tree[key]) {
+          items.push(
+            <div key={Math.random()} className="innerCol col s4 no-pad-mar">
+              <div className="wrapper">{this.parseNestedObj(tree[key][j])}</div>
+            </div>
+          );
+        }
+        rows.push(
+          <div key={Math.random()} className="row innerRow no-pad-mar">
+            {items}
           </div>
         );
+      } else if (this.isObject(tree[key])) {
+        rows.push(
+          <div key={Math.random()}>
+            <span className="col-title">
+              <b>{key}: </b>
+            </span>
+            <span>{this.parseNestedObj(tree[key])}</span>
+          </div>
+        );
+      } else {
+        console.log(key, tree[key]);
+        if (key == "source") {
+        } else if (key == "name") {
+          rows.push(
+            <a key={Math.random()} href={tree["link"]}>
+              {tree[key]}
+            </a>
+          );
+        } else if (key == "cast_link") {
+          rows.push(
+            <div key={Math.random()}>
+              <a href={tree["cast_link"]}>View Full Cast</a>
+            </div>
+          );
+        } else if (key == "image") {
+          rows.push(
+            <div key={Math.random()}>
+              <img className="posterImg" src={tree[key]} width="100px" />
+            </div>
+          );
+        } else if (key == "link") {
+          rows.push(<div key={Math.random()} />);
+        } else if (key == "people_also_search_for_link") {
+          <div key={Math.random()}>
+            <a href={tree["people_also_search_for_link"]}>
+              people also search for link
+            </a>
+          </div>;
+        } else {
+          rows.push(
+            <div key={Math.random()} className="item">
+              <span className="col-title">
+                <b>{key}</b>:
+              </span>
+              <span>{tree[key]}</span>
+            </div>
+          );
+        }
       }
     }
-    return <div className="row">{rows}</div>;
+    return <div className="">{rows}</div>;
   };
 
   graphParse = graph => {
+    console.log(graph);
     return this.parseNestedObj(graph);
   };
 
@@ -82,7 +149,7 @@ class Main extends Component {
               })}
             </div>
             <div className="col l4">
-              {this.graphParse(this.state.data.knowledge_graph)}
+              <div>{this.graphParse(this.state.data.knowledge_graph)}</div>
             </div>
           </div>
         ) : (
